a83932f0a532000bdae1dfce2d139a5dd59b12c2
3nori <mino.note@gmail.com>
2023-11-02 13:24:52 +0900

    モーダル箇所が表示されない

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 //モーダル部分
-$(function(){
-    $('.modalopen').each(function (){
-        $(this).on('click' , function(){
-            var target =$(this).date('target');
-            var modal = document.getElementById(target);
-            console.log(modal);
-            $(modal).fadeIn();
-            return false;
+$(function () {
+    $('.modalopen').each(function () {
+      $(this).on('click', function () {
+        var target = $(this).data('target');
+        var modal = document.getElementById(target);
+        console.log(modal);
+        $(modal).fadeIn();
+        return false;
         });
     });
 
-    $( '.modal-inner' ).click( function(){
+    //モーダル領域をクリックでフェードアウトする
+    $( '.modal-main' ).click( function(){
         $( this ).fadeOut();
-        return false;
+    });
+    //がしかし、画像クリックでキャンセルさせる
+    $('.modal-inner').on('click',function( e ){
+        e.stopPropagation();
     });
 });
