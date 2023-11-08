1048b694e973acdec72a27150cb2ff8de6b72991
3nori <mino.note@gmail.com>
2023-11-08 10:28:32 +0900

    id変更前

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
-//モーダル部分
-$(function () {
-    $('.modalopen').each(function () {
-      $(this).on('click', function () {
-        var target = $(this).data('target');
-        var modal = document.getElementById(target);
-        console.log(modal);
-        $(modal).fadeIn();
+//モーダル
+$(function(){
+    var open = $('.modal-open'),
+        container = $('.modal-inner');
+
+    open.on('click',function(){
+        container.addClass('active');
         return false;
-        });
     });
 
-    //モーダル領域をクリックでフェードアウトする
-    $( '.modal-main' ).click( function(){
-        $( this ).fadeOut();
+    $(document).on('click',function(e){
+        if(!$(e.target).closets('.inner-content').length){
+            container.removeClass('active');
+        }
     });
-    //がしかし、画像クリックでキャンセルさせる
-    $('.modal-inner').on('click',function( e ){
-        e.stopPropagation();
-    });
+
+});
+
+
+//画像切り替え
+$(function()
+{
+	$(".sub").eq(0).addClass("select");
+	$(".sub-img").click(function()
+	{
+		var img = $(this).attr("src");
+
+		$(".sub").removeClass("select");
+		$(this).parent().addClass("select");
+
+		$(".main_img").fadeOut(500, function()
+		{
+			$(this).attr("src", img),
+			$(this).fadeIn(500)
+		});
+	});
 });
